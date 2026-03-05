fa4d1908b4e9062ae9e12106462da03f08178f9b
Miguel <mhalys91@gmail.com>
2026-03-05 22:19:32 +0100

    feat(add-transaction): better ux

--- apps/mobile/app/(auth)/(tabs)/transactions/[id].tsx
+++ apps/mobile/app/(auth)/(tabs)/transactions/[id].tsx
@@ -1,16 +1,1 @@
-import { Redirect, useLocalSearchParams } from "expo-router";
-
-type EditParams = {
-  id?: string | string[];
-};
-
-export default function LegacyTabsEditTransactionRoute() {
-  const params = useLocalSearchParams<EditParams>();
-  const id = Array.isArray(params.id) ? params.id[0] : params.id;
-
-  if (!id) {
-    return <Redirect href="/(auth)/(tabs)/transactions" />;
-  }
-
-  return <Redirect href={`/(auth)/transactions/${id}`} />;
-}
+export { default } from "../../transactions/[id]";
--- apps/mobile/app/(auth)/(tabs)/transactions/create.tsx
+++ apps/mobile/app/(auth)/(tabs)/transactions/create.tsx
@@ -1,12 +1,1 @@
-import { Redirect, useLocalSearchParams } from "expo-router";
-
-type CreateParams = {
-  type?: string | string[];
-  date?: string | string[];
-  kind?: string | string[];
-};
-
-export default function LegacyTabsCreateTransactionRoute() {
-  const params = useLocalSearchParams<CreateParams>();
-  return <Redirect href={{ pathname: "/(auth)/transactions/create", params }} />;
-}
+export { default } from "../../transactions/create";
